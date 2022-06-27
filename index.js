caee35c60809536c4096227f87310afa48d2689f
Boeieruurd <94339712+Boeieruurd-Noob@users.noreply.github.com>
2022-06-27 22:05:46 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,24 +8,41 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Sends instruction when starting the bot
+
 bot.command('start', ctx => {
   console.log(ctx.from)
   bot.telegram.sendMessage(ctx.chat.id, 'Type "yieldly apy" to get the current apy of the YLDY/YLDY Pool.', {
   })
 })
 
-//fetches yieldly apy
+//bot action
 bot.hears('yieldly apy', ctx => {
-  var yldyapy;
+
   console.log(ctx.from)
+
+//fetches apy values
+
   axios.get(`https://app.yieldly.finance/staking/pools/v3/233725850`)
   .then(response => {
     console.log(response.data)
+
+//defines apy / tvl and rounds the numbers gotten from yieldly.
+
     apy = (Math.round(response.data.apy * 100) / 100)
     tvlusd = (Math.round(response.data.tvlUSD))
     fixedtvlusd = (tvlusd.toLocaleString('en-US'))
+
+//defines message_id to quote respond on telegram, and defines the message to respond.
+
     const message_id = ctx.message.message_id
-    const message =`YLDY/YLDY APY is: ${apy}% TVL: ${fixedtvlusd} USD.`
+    const message =
+`
+YLDY/YLDY APY is: ${apy}%
+TVL: ${fixedtvlusd} USD.
+
+`
+
     ctx.reply(message, { reply_to_message_id: message_id }) ,{
     }
   })
